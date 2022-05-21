6f3fc8775291586cc54e634739c708e57be6b221
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-21 18:12:37 +0300

    #1

---
+++ Task_17_Degree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_17_Degree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxLimitRandom = 1000;
+            int minLimitRandom = 0;
+            Random random = new Random();
+            int limitForMultiplication = random.Next(minLimitRandom, maxLimitRandom);
+            
+            int multiplyNumber = 2;
+            int degree = 1;
+            int sumMultiply = multiplyNumber;
+            
+            for (int i = multiplyNumber; i < limitForMultiplication; i *= multiplyNumber)
+            {
+                sumMultiply *= multiplyNumber;
+                degree++;
+            }
+
+            Console.WriteLine($"Исходное число: {limitForMultiplication} \nСтепень: {degree} \nЧисло {multiplyNumber} в степени {degree}: {sumMultiply}");
+        }
+    }
+}
